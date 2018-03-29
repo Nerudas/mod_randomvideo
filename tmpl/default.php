09c004921c444cdbcfc41f53a04547acf6ec4fc5
Septdir <septdir@gmail.com>
2018-03-29 17:10:04 +0300

    Change version to 1.0.1

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Random Video Module
- * @version    1.0.0
+ * @version    1.0.1
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
